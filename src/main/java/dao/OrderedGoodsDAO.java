b21c427fe40c22bb718369fb95397ff0ef5197ff
Sui Nomura <su-nomura@sr-net.co.jp>
2025-06-19 14:33:47 +0900

    OrderedGoodsDAO作成

--- src/main/java/dao/OrderedGoodsDAO.java
+++ src/main/java/dao/OrderedGoodsDAO.java
@@ -2,6 +2,10 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class OrderedGoodsDAO {
 
@@ -11,6 +15,7 @@ public class OrderedGoodsDAO {
 	private static String USER = "root";
 	private static String PASS = "root123";
 
+	// データベース接続情報を利用してデータベースに接続するクラスメソッド
 private static Connection getConnection() {
 		try {
 			Class.forName(RDB_DRIVE);
@@ -22,6 +27,57 @@ private static Connection getConnection() {
 		}
 	}
 
+//引数のユーザーIDが購入した商品のArrayListを返すメソッド
+public ArrayList<Goods> orderedGoods(String buyuser_id){
+
+	Connection con = null;
+	Statement smt = null;
+	
+	String sql = "SELECT * FROM goodsinfo WHERE buyuser_id = "+ buyuser_id + "ORDER BY buy_date";
+	
+	//
+	ArrayList<Goods> orderedGoodsList = new ArrayList<Goods>();
+
+try {
+	con = getConnection();
+	smt = con.createStatement();
+
+	ResultSet rs = smt.executeQuery(sql);
+	
+	while (rs.next()) {
+		Goods goods = new Goods();
+		
+		Goods.setGoodsId(rs.getString("goods_id"));
+		Goods.setSelluserId(rs.getString("selluser_id"));
+		Goods.setImgPath(rs.getString("img_path"));
+		Goods.setGoodsName(rs.getString("name"));
+		Goods.setPrice(rs.getString("price"));
+		Goods.setQuantity(rs.getString("quantity"));
+		Goods.setCategory(rs.getString("category"));
+		Goods.setGoodsMemo(rs.getString("goods_memo"));
+		Goods.setStatus(rs.getString("status"));
+		Goods.setExhibitDate(rs.getString("exhibit_date"));
+		Goods.setBuyDate(rs.getString("buy_date"));
+		Goods.setBuyuserId(rs.getString("buyuser_id"));
+
+} catch (Exception e) {
+	throw new IllegalStateException(e);
+} finally {
+	if (smt != null) {
+		try {
+			smt.close();
+		} catch (SQLException ignore) {
+		}
+	}
+	if (con != null) {
+		try {
+			con.close();
+		} catch (SQLException ignore) {
+		}
+	}
+}
+return orderedGoodsList;
+}
 
 
 
