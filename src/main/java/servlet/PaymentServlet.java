fda4ec0748a466bf9a71f1017125eb4b20813e9d
yamanatsu0702 <n-yamashita@sr-net.co.jp>
2025-06-23 13:16:21 +0900

    Update PaymentServlet.java
    アノテーション追加

--- src/main/java/servlet/PaymentServlet.java
+++ src/main/java/servlet/PaymentServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/payment")
 public class PaymentServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
