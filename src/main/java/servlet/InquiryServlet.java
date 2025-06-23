576120f585f7133e409bce0c90c5a6be397b40eb
kanda-it <kanda-it@DESKTOP-FM6J0C3>
2025-06-23 11:51:38 +0900

    アノテーション変更

--- src/main/java/servlet/InquiryServlet.java
+++ src/main/java/servlet/InquiryServlet.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("inquiery")
+@WebServlet("/inquiery")
 public class InquiryServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
