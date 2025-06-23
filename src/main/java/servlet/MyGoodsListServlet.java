cb443dad6c43d7f2919e266b8e3f6bdd36c7a1ea
yamanatsu0702 <n-yamashita@sr-net.co.jp>
2025-06-23 13:15:06 +0900

    Update MyGoodsListServlet.java
    アノテーション追加

--- src/main/java/servlet/MyGoodsListServlet.java
+++ src/main/java/servlet/MyGoodsListServlet.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+@WebServlet("/mygoodsList")
 public class MyGoodsListServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
