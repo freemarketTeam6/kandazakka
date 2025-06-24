deb3fad1cf2b7d6d198e7187c79c19098a799f91
kanda-it <kanda-it@172.16.1.25>
2025-06-24 14:21:43 +0900

    商品一覧をDoPostにも対応

--- src/main/java/servlet/MyGoodsListServlet.java
+++ src/main/java/servlet/MyGoodsListServlet.java
@@ -60,5 +60,51 @@ public class MyGoodsListServlet extends HttpServlet {
 	
 	
 	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String cmd = "";
+		String error = "";
+		
+		try {
+		
+		// セッションからUserオブジェクトを取得してセッション切れの判定
+					HttpSession session = request.getSession();
+					User user = (User) session.getAttribute("user");
+					if (user == null) {
+						cmd = "logout";
+						error = "セッション切れの為、出品一覧を表示できません。";
+						return;
+					}
+		
+			//Useridを変数に格納
+			String userid=user.getUserid();
+					
+			//GoodsDAOのオブジェクトを作成
+			GoodsDAO goodsDaoObj=new GoodsDAO();		
+			
+			
+			//useridをもとに出品したGoodsオブジェクトのArrayListを取得
+			ArrayList<Goods> myGoodsList = new ArrayList<Goods>();
+			myGoodsList=goodsDaoObj.selectGoodsBySelluser(userid);
+			
+			//リクエストスコープに出品一覧GoodsのArrayListを送る
+			request.setAttribute("myGoodsList", myGoodsList);	
+		}catch (IllegalStateException e) {
+			cmd = "mypage";
+			error = "DB接続エラーの為、出品一覧を表示できません。";
+		} finally {
+			if (error.equals("")) {
+				//myGoodsList.jspに遷移
+				request.getRequestDispatcher("/view/myGoodsList.jsp").forward(request, response);
+			} else {
+				request.setAttribute("cmd", cmd);
+				request.setAttribute("error", error);
+				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+			}
+		}
+		
+		
+		}
 				
 }
