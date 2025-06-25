9d34fbb691212136e98b56b497da991348921eb6
kanda-it <kanda-it@DESKTOP-MMLSDH1>
2025-06-25 10:34:01 +0900

    goodsDetail系、修正しました。

--- src/main/java/servlet/GoodsDetailServlet.java
+++ src/main/java/servlet/GoodsDetailServlet.java
@@ -17,18 +17,18 @@ public class GoodsDetailServlet extends HttpServlet {
 		String cmd = "";
 
 		try {
-			// cmd取得（商品情報変更のため）
-			cmd = (String) request.getParameter("cmd");
-
 			// 表示する商品情報を格納するGoodsオブジェクトを生成
 			Goods goods = new Goods();
 
 			// GoodsDAOクラスのオブジェクトを生成
 			GoodsDAO goodsDao = new GoodsDAO();
+			
+			// cmd取得（フォワード先を変更するため）
+			cmd = (String) request.getParameter("cmd");
 
 			// 画面から送信されるgoods_id情報を受け取る
 			int goods_id = Integer.parseInt(request.getParameter("goods_id"));
-
+			
 			// 商品情報を取得
 			goods = goodsDao.selectGoodsByGoodsID(goods_id);
 
@@ -47,12 +47,14 @@ public class GoodsDetailServlet extends HttpServlet {
 			error = "DB接続エラーのため、詳細画面は表示できませんでした。";
 			cmd = "logout";
 		} finally {
-			// エラーがなければ「goodsDetail.jsp」へフォワード
 			if (error.equals("")) {
+				// エラーがなくcmdがdetailなら「goodsDetail.jsp」へフォワード
 				if (cmd.equals("detail")) {
 					request.getRequestDispatcher("/view/goodsDetail.jsp").forward(request, response);
+				// エラーがなくcmdがupdateなら「changeGoods.jsp」へフォワード
 				} else if(cmd.equals("update")){
 					request.getRequestDispatcher("/view/changeGoods.jsp").forward(request, response);
+				// エラーがなくcmdが上記以外なら「shipping.jsp」へフォワード
 				}else {
 					request.getRequestDispatcher("/view/shipping.jsp").forward(request, response);
 				}
